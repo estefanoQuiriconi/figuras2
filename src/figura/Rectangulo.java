cc98a0f315ea4002344ccbdf842be6a367f1c48a
Estéfano Quiriconi <estefanoquiriconi@gmail.com>
2021-08-19 18:01:22 -0300

    Versión Inicial del proyecto Figura

---
+++ src/figura/Rectangulo.java
@@ -0,0 +1,14 @@
+package figura;
+
+public class Rectangulo {
+	
+	double ancho, alto;
+
+	public Rectangulo(double ancho, double alto) {
+		super();
+		this.ancho = ancho;
+		this.alto = alto;
+	}
+	
+
+}
